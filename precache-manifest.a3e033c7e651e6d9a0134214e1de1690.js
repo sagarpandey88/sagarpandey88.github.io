ee0d65f68a0ffc01d9cc2bbcf67bce2544004da0
Sagar Pandey <sagar.pandey@ey.com>
2020-10-12 20:31:14 +0530

    Updates

---
+++ precache-manifest.a3e033c7e651e6d9a0134214e1de1690.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c6e0e9a5ac18d8a8a944f7550c91f06",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "228660a8fd26f7e3aa1e",
+    "url": "/my-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/my-app/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "228660a8fd26f7e3aa1e",
+    "url": "/my-app/static/js/main.61bdfae7.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
